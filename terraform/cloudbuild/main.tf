005c3c27611781a917dc5fb0858edb17a272bf1b
Phu Le <phu.le.dev@gmail.com>
2021-04-24 07:35:22 +0000

    MODIFY pull reques trigger

--- terraform/cloudbuild/main.tf
+++ terraform/cloudbuild/main.tf
@@ -3,20 +3,17 @@ provider "google" {
 }
 
 resource "google_cloudbuild_trigger" "gcp_gitops_terraform_push_trigger" {
-  name = "gcp-gitops-terraform-push-trigger"
+  name = "gcp-gitops-terraform-pr-trigger"
   description = "Push trigger for Repository: https://github.com/p-le/gcp-gitops-terraform"
 
   github {
     owner = "p-le"
     name = "gcp-gitops-terraform"
-    push {
-      branch = ".*"
+    pull_request {
+      branch = "^dev|prod$"
+      comment_control = "COMMENTS_ENABLED"
     }
   }
-
-  tags = [
-    "github-default-push-trigger"
-  ]
 
   filename = "cloudbuild.yaml"
 }
